4a6f39f00116cc8fc85a104c9b7320b9bc83ed63
ph4r05 <ph4r05@ph4.maleus>
2013-02-01 19:49:30 +0100

    copies original JAXB entities to this project

---
+++ src/main/java/com/phoenixpho/phoenixksoap2/Sync.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.phoenixpho.phoenixksoap2;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sun.text.normalizer.Replaceable;
+
+/**
+ * Fetches all JAXB generated entities from original project and downloads it here.
+ * @author ph4r05
+ */
+public class Sync {
+    public static final String SRC_PATH = "/home/ph4r05/workspace/PhoenixSoap/phoenix-soap-parent/phoenix-soap-app/target/generated-sources/jaxb/com/phoenix/soap/beans";
+    public static final String DST_PATH = "/home/ph4r05/workspace/PhoenixKsoap2/src/main/java/com/phoenix/soap/beans";
+    
+    public Sync() {
+        // nothing to do
+    }
+    
+    public static void main( String[] args )
+    {  
+        File srcDir = new File(SRC_PATH);
+        if (srcDir.exists()==false || srcDir.isDirectory()==false || srcDir.canRead()==false){
+            System.err.println("Something is wrong with source directory: " + SRC_PATH);
+            System.exit(1);
+        }
+        
+        File dstPath = new File(DST_PATH);
+        if (dstPath.exists()==false || dstPath.isDirectory()==false || dstPath.canWrite()==false){
+            System.err.println("Something is wrong with destination directory: " + DST_PATH);
+            System.exit(1);
+        }
+        
+        for (File child : srcDir.listFiles()){
+            if (child.isFile()==false) continue;
+            if ("package-info.java".equals(child.getName())) continue;
+            System.out.println("Found file: " + child.getName());
+            
+            try {
+                Files.copy(child.toPath(), new File(DST_PATH + "/" + child.getName()).toPath(), StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException ex) {
+                System.err.println("Problem with toPath conversion");
+                ex.printStackTrace(System.err);
+            }
+        }
+    }
+}
